352d841e353d6c1d45758caf366b8b324e1c6e59
kobepeng <pengkobe123@gmail.com>
2018-01-23 18:02:25 +0800

    fix bug 03.2

--- v0.3.js
+++ v0.3.js
@@ -11,3 +11,7 @@ fix one bug in fixbug-03 v0.3`);
 // 虽然已经 fix one bug 了 但是还有 03.1
 console.log(`
 fix one bug in fixbug-03.1 v0.3`);
+
+// 虽然已经 fix 2 bug 了 但是还有 03.2
+console.log(`
+fix one bug in fixbug-03.2 v0.3`);
